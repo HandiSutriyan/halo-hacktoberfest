855bd27ea9ad9339b8f76f6cf0efd73ed787e974
HandiSutriyan <handisutrian@gmail.com>
2020-01-24 18:32:41 +0700

    fixing-bug

---
+++ js/getdata.js
@@ -0,0 +1,53 @@
+$.getJSON("../data/data.json", function(result){
+    console.log(result);
+    let card = document.getElementById('card');
+
+    $.each(result, function(i){
+
+      let grid = document.createElement('div');
+      grid.setAttribute('class','l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-3');
+
+      let container = document.createElement('div');
+      container.setAttribute('class','card');
+
+     let img_container= document.createElement('div');
+     img_container.setAttribute('class','card-img-container');
+
+     let img = document.createElement('img');
+     img.setAttribute('src','/img/no-thumbnail.jpg');
+     img.setAttribute('class','image');
+
+     let middle = document.createElement('div');
+     middle.setAttribute('class','middle');
+
+     let text = document.createElement('div');
+     text.setAttribute('class','text');
+
+     let link = document.createElement('a');
+     link.setAttribute('href','#');
+     link.textContent = "Lihat";
+
+     let cardContainer = document.createElement('div');
+     cardContainer.setAttribute('class','card-container');
+
+     let h4 = document.createElement('h4');
+     h4.textContent = result[i].Tim;
+
+     let p = document.createElement('p');
+     p.textContent = result[i].Kelas;
+
+
+     img_container.appendChild(img);
+     text.appendChild(link);
+     middle.appendChild(text);
+     img_container.appendChild(middle);
+
+     container.appendChild(img_container);
+     
+     cardContainer.appendChild(h4);
+     cardContainer.appendChild(p);
+     container.appendChild(cardContainer);
+     grid.appendChild(container);
+     card.appendChild(grid);
+    });
+  });
